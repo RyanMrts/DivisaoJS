6528186e518c0d2b60d56251c68b5b4dd1e70dd9
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-10-20 22:48:28 -0300

    Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+let devidendo, divisor, quociente;
+
+alert ("== DIVISÂO ==")
+devidendo = prompt ("Digite o devidendo:")
+divisor = prompt ("Digite o divisor: ")
+
+if (divisor == 0)
+{
+    alert("Não é possível dividir por 0.")
+}
+else 
+{
+    resultado = Number(devidendo) / Number(divisor)
+    alert (`${devidendo} dividido por ${divisor} é ${resultado}`)
+}
+
+Boolean = alert (quociente = Number(devidendo) / Number(divisor))
